5bb61e7241e57155f52c15c249fe077e80df1e74
danielbatres <danielbatresuser@gmail.com>
2023-10-17 10:53:30 -0600

    Time complexity evaluation for linear search

--- algorithms/linearSearch.js
+++ algorithms/linearSearch.js
@@ -1,15 +1,18 @@
 /**
- * Time complexity --> O()
+ * Time complexity --> O( n + 3 ) --> O(n)
  *
  * Spacial complexity --> O()
+ * 
+ * Ot() = time
+ * Os() = space
  */
 
 function linearSearch(array, key) {
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] === key) {
-      return index;
+  for (let index = 0; index < array.length; index++) { // Ot(n)
+    if (array[index] === key) { // Ot(1)
+      return index; // Ot(1)
     }
   }
 
-  return -1;
+  return -1; // Ot(1)
 }
