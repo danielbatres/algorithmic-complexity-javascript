e5b5cb54448a91f4a0e4b57ae9f68119b579d800
danielbatres <danielbatresuser@gmail.com>
2023-10-17 10:43:40 -0600

    Calculations and criteria for Big O notation

---
+++ big-o-calculation.js
@@ -0,0 +1,35 @@
+// Big O calculation for time complexity
+
+let bar = 'text' // O(1)
+
+if () {} // O(1)
+
+for () {} // O(n)
+
+while() {} // O(n)
+
+for() { for() {} } // O(n^2)
+
+// Big O calculation for space complexity
+
+let text = 'text' // O(1)
+
+if () {} // O(1)
+
+for() {} // O(1)
+
+let result = [1, 2, ..., n] // O(n)
+
+let dimensional = [ // O(n^2)
+  [2, 4],
+  [6, 8],
+  [10, 12]
+]
+
+// Simplify the notation
+
+// O(2n) ---> O(n)
+
+// O(50) ---> O(1)
+
+// O(n^2 + 50) ---> O(n^2)
